0f8d0bb9a972db736bf8ba361fa11098bd93848a
Robin-git <guerin28.robin@gmail.com>
2022-08-22 12:21:39 +0200

    feature: migrate manager

---
+++ db/migrations/000001_init_schema.up.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    password_hash BYTEA NOT NULL,
+    password_salt BYTEA NOT NULL,
+    refresh_token VARCHAR,
+    token_created DATE,
+    token_expires DATE
+);
+
+COMMIT;
