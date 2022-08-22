3418dbd18547f08dccce01fb2b1cf51515574f9e
Robin-git <guerin28.robin@gmail.com>
2022-08-22 12:23:35 +0200

    fix: reorder folders

--- sql/2_create_user_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    username VARCHAR NOT NULL,
-    password_hash BYTEA NOT NULL,
-    password_salt BYTEA NOT NULL,
-    refresh_token VARCHAR,
-    token_created DATE,
-    token_expires DATE
-);
