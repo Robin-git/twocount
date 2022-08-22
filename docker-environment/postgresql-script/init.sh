5e6c16edc723fdb6b4027bb922b2545881a5f6ad
Robin-git <guerin28.robin@gmail.com>
2022-08-22 14:40:53 +0200

    fix: transaction in init database sh

--- docker-environment/postgresql-script/init.sh
+++ docker-environment/postgresql-script/init.sh
@@ -2,6 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$POSTGRES_DB"<<-EOSQL
+   BEGIN;
    CREATE DATABASE twocount;
    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+   COMMIT;
 EOSQL
